832a585953599e5c9b67693b07e93333d33380a5
kyuuzou <newita@gmail.com>
2023-06-10 05:53:28 +0900

    Add demo mode to USB_ComputeUnlitTexture shader.

--- Assets/Scripts/Shader Controllers/USBComputeUnlitTexture.cs
+++ Assets/Scripts/Shader Controllers/USBComputeUnlitTexture.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using UnityEngine;
 
 public class USBComputeUnlitTexture : MonoBehaviour {
@@ -6,13 +7,36 @@ public class USBComputeUnlitTexture : MonoBehaviour {
     private ComputeShader shader;
 
     [SerializeField]
-    private Texture2D colorTexture;
-    
+    private Texture2D colorTextureA;
+
+    [SerializeField]
+    private Texture2D colorTextureB;
+
     [SerializeField]
     private int textureSize = 256;
 
     [SerializeField]
     private RenderTexture mainTexture;
+
+    [SerializeField]
+    private bool demoMode = true;
+
+    private IEnumerator DemoModeCoroutine(){
+        do {
+            yield return new WaitForSeconds(1.0f);
+            this.SetTexture(this.colorTextureB);
+
+            yield return new WaitForSeconds(1.0f);
+            this.SetTexture(this.colorTextureA);
+        } while (true);
+    }
+
+    private void SetTexture(Texture2D colorTexture) {
+        shader.SetTexture(0, "ColorTexture", colorTexture);
+
+        // generate the thread group to process the texture
+        shader.Dispatch(0, textureSize / 8, textureSize / 8, 1);
+    }
 
     private void Start() {
         mainTexture = new RenderTexture(textureSize, textureSize, 0, RenderTextureFormat.ARGB32);
@@ -21,13 +45,15 @@ public class USBComputeUnlitTexture : MonoBehaviour {
 
         Renderer renderer = GetComponent<Renderer>();
         renderer.enabled = true;
-        
+
         // 0 = index of the kernel, in this case CSMain
         shader.SetTexture(0, "Result", mainTexture);
-        shader.SetTexture(0, "ColorTexture", colorTexture);
         renderer.material.SetTexture("_MainTex", mainTexture);
-        
-        // generate the thread group to process the texture
-        shader.Dispatch(0, textureSize / 8, textureSize / 8, 1);
+
+        this.SetTexture(this.colorTextureA);
+
+        if (this.demoMode) {
+            this.StartCoroutine(this.DemoModeCoroutine());
+        }
     }
 }

bd1cd011d4c3cf352b871d8220c3cbd5ac2e012c
kyuuzou <newita@gmail.com>
2023-06-12 00:50:57 +0900

    Implement a property changer for colors.

---
+++ Assets/Scripts/Properties/ColorShaderPropertyChanger.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+
+public class ColorShaderPropertyChanger : ShaderPropertyChanger<Color> {
+
+    [SerializeField]
+    private float minHue = 0.0f;
+
+    [SerializeField]
+    private float maxHue = 1.0f;
+
+    [SerializeField]
+    private float saturation = 1.0f;
+
+    [SerializeField]
+    private float brightness = 1.0f;
+
+    [SerializeField]
+    private float speed = 1.0f;
+
+    protected override void Update() {
+        float hue = Mathf.Abs(Mathf.Sin(Time.time * this.speed)) * (maxHue - minHue) + minHue;
+        Debug.Log(Color.HSVToRGB(hue, saturation, brightness));
+        this.SetProperty(Color.HSVToRGB(hue, saturation, brightness));
+    }
+}
